f6970cf05cca923908c93e11f68faf722b0dc52b
kamrankausher <kamrankausher@gmail.com>
2026-04-08 03:11:10 +0530

    good message

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
 from devflow.scanner import scan_directory
 from devflow.rules import analyze_commit_message, analyze_staged_files
+from devflow.git_analyzer import (
+    check_main_branch,
+    check_untracked_files,
+    check_large_files
+)
 import sys
 import subprocess
 
 # -------------------------------
-# STEP 1: Run scanner
+# STEP 1: Security Scan (BLOCK)
 # -------------------------------
 issues = scan_directory(".")
 
@@ -17,12 +22,11 @@ if issues:
     sys.exit(1)
 
 # -------------------------------
-# STEP 2: Get commit message
+# STEP 2: Get commit message (FIXED)
 # -------------------------------
 try:
-    commit_msg = subprocess.check_output(
-        ["git", "log", "-1", "--pretty=%B"]
-    ).decode().strip()
+    with open(".git/COMMIT_EDITMSG", "r") as f:
+        commit_msg = f.read().strip()
 except:
     commit_msg = ""
 
@@ -44,8 +48,13 @@ warnings = []
 warnings += analyze_commit_message(commit_msg)
 warnings += analyze_staged_files(staged_files)
 
+# 🔥 NEW INTELLIGENCE
+warnings += check_main_branch()
+warnings += check_untracked_files()
+warnings += check_large_files(staged_files)
+
 # -------------------------------
-# STEP 5: Show warnings (DO NOT BLOCK)
+# STEP 5: Show warnings
 # -------------------------------
 if warnings:
     print("\n⚠️ Suggestions:\n")
@@ -54,16 +63,3 @@ if warnings:
 
 print("\n✅ Commit allowed")
 sys.exit(0)
-
-from devflow.git_analyzer import (
-    check_main_branch,
-    check_untracked_files,
-    check_large_files
-)
-
-# -------------------------------
-# STEP 6: Git workflow analysis
-# -------------------------------
-warnings += check_main_branch()
-warnings += check_untracked_files()
-warnings += check_large_files(staged_files)
